75f8b440c16838ad193dab53a5c7b4911ab23a59
global-crypto-dev <125596795+global-crypto-dev@users.noreply.github.com>
2023-05-02 14:45:35 +0300

    Init ver.

---
+++ Shop.Shared.Abstractions/Commands/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Shared.Abstractions.Commands
+{
+    public interface ICommandHandler
+    {
+        public interface ICommandHandler<in TCommand> where TCommand : class, ICommand
+        {
+            Task HandleAsync(TCommand command);
+        }
+    }
+}
